82d951bfc28da1161251039e5b0339029379e97e
salvadorsanchezmajos <125193197+salvadorsanchezmajos@users.noreply.github.com>
2026-01-14 16:27:46 +0100

    Revert "Add Pac-Man radius offset to Y rendering position"
    This reverts commit 792fc8e70b86980486d1a476c477c36fb51ba726.

--- game.js
+++ game.js
@@ -1533,9 +1533,8 @@ class PacManGame {
         }
 
         // Draw Pac-Man - convert grid position to pixel position
-        const radius = TILE_SIZE / 2 - 1;
         const px = this.pacman.x * TILE_SIZE + TILE_SIZE / 2;
-        const py = this.pacman.y * TILE_SIZE + TILE_SIZE / 2 + radius;
+        const py = this.pacman.y * TILE_SIZE + TILE_SIZE / 2;
 
         // Pac-Man glow
         this.ctx.shadowBlur = 10;
